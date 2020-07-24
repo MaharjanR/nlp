e0cf6af0a9f4d120a397a5e96a2f026924bfbf57
Ritesh <catalight.github.io>
2020-07-24 09:49:28 -0400

    added comments on server

--- src/server/index.js
+++ src/server/index.js
@@ -28,16 +28,21 @@ app.get("/", function (req, res) {
     res.sendFile("dist/index.html");
 });
 
+// runs the function when post title is called from client
 app.post("/title", async (req, res) => {
     try {
+        // calls the api by passing API key, type i.e URL or word and the text which is url or word
         const result = await axios.post(
             `http://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&lang=en&${req.body.type}=${req.body.title}`
         );
 
+        // stores the result of the call in data
         const { data } = result;
         const { code } = data.status;
 
+        // status code 200 is failed so runs following if not failed
         if (code !== "200") {
+            // stores the below field in its own variable
             const { score_tag } = data;
             const { agreement } = data;
             const { subjectivity } = data;
@@ -53,6 +58,7 @@ app.post("/title", async (req, res) => {
                 irony,
             };
         } else {
+            // if error occurs then change the response to false
             sentiment = false;
         }
         res.end("It worked!");
@@ -61,10 +67,13 @@ app.post("/title", async (req, res) => {
     }
 });
 
+// calls the function on get sentiment method
 app.get("/sentiment", (req, res) => {
+    // sends the sentiment variable
     res.send(sentiment);
 });
 
+// setting up the port in 3000 if no .env.port present in .env file
 const port = process.env.port || 3000;
 
 // designates what port the app will listen to for incoming requests
